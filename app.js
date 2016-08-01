5a07f8373906fbed0601ca4b9d8e000134828556
LadislavSzolik <ladislavszolik@Ladislavs-MacBook-Pro.local>
2016-08-01 20:50:11 +0200

    Created new version with simple editing

--- app.js
+++ app.js
@@ -1,105 +1,69 @@
-const clipboard = require('electron').clipboard
-var fs = require('fs');
+// const clipboard = require('electron').clipboard
+//var fs = require('fs');
 
 var simpleStepApp = angular.module('simpleStepApp', ['ngRoute', 'hc.marked','ngSanitize']);
 
 simpleStepApp.config(['$routeProvider','markedProvider', function($routeProvider, markedProvider){
   markedProvider.setOptions({gfm: true});
 
-  $routeProvider.when('/inputArea', {
-    templateUrl: 'inputArea.html'
+  $routeProvider.when('/startView', {
+    templateUrl: 'startView.html'
+  })
+  .when('/editView',{
+    templateUrl: 'editView.html'
+  })
+  .when('/previewView', {
+    templateUrl: 'previewView.html'
   })
   .when('/list', {
     templateUrl: 'list.html'
   }).otherwise({
-    redirectTo: '/inputArea'
+    redirectTo: '/startView'
   });
 }]);
 
-simpleStepApp.controller('textAreaController',['$scope','simpleStapService', 'marked', function($scope, simpleStapService, marked){
 
-  $scope.steps = {textInput : simpleStapService.getInputText()};
+simpleStepApp.controller('editViewController',['$scope', 'simpleStapService', function($scope, simpleStapService){
+  $scope.inputContent = simpleStapService.inputContent;
 
-  // img example
-  //$scope.testText = marked("![an image](/Users/ladislavszolik/Pictures/me.png)");
-
-  $scope.runButton = function() {
-    simpleStapService.saveSteps($scope.steps.textInput);
+  $scope.convert = function() {
+    simpleStapService.convertIntoListOfItems($scope.inputContent);
   }
 
 }]);
 
+simpleStepApp.controller('previewViewController',['$scope', 'simpleStapService', function($scope, simpleStapService){
+  $scope.objectList = simpleStapService.objectList;
 
+  $scope.copyText = function(stepText) {
+    //clipboard.writeText(stepText);
+  };
 
-simpleStepApp.controller('stepListController', ['$scope', 'simpleStapService', function($scope, simpleStapService) {
-    var stepList = this;
-    stepList.steps = simpleStapService.getSteps();
-
-    // $scope.backButton = function() { simpleStapService.updateSteps(stepList.steps);}
-
-    $scope.copyText = function(stepText) {
-      clipboard.writeText(stepText);
+  $scope.toggleDone = function(index) {
+    if(simpleStapService.objectList[index].done) {
+      simpleStapService.objectList[index].done = false;
+    } else {
+      simpleStapService.objectList[index].done = true;
     }
-
-  }]);
-
-  simpleStepApp.service('simpleStapService', ['marked', function(marked) {
-    this.inputText = [];
-    this.steps = [];
-
-
-    this.saveSteps = function (inputText) {
-      this.inputText = inputText;
-      this.steps = [];
-      this.tranformTextAreaToObjects();
-    }
-
-    this.tranformTextAreaToObjects = function() {
-      for(step in this.inputText) {
-        this.steps.push({text: marked(this.inputText[step]), done:false});
-      }
-
-    }
-
-    this.getInputText = function() {
-      return this.inputText;
-    }
-
-    this.updateSteps = function(listOfSteps) {
-      this.inputText = [];
-
-      for(stepItem in listOfSteps) {
-        this.inputText.push(listOfSteps[stepItem].text);
-      }
-      console.log(this.inputText);
-    }
-
-    this.getSteps = function() {
-      if(this.steps.length < 1){
-        return [{text:"Empty list", done: false}];
-      }
-      return this.steps;
-    }
-
+    $scope.objectList = simpleStapService.objectList;
+  };
 }]);
 
-simpleStepApp.directive("contenteditable", function() {
-  return {
-    restrict: "A",
-    require: "ngModel",
-    link: function(scope, element, attrs, ngModel) {
 
-      function read() {
-        ngModel.$setViewValue(element.html());
+  simpleStepApp.service('simpleStapService', ['marked', function(marked) {
+
+    this.inputContent = "[] todo [] todo [] todo" ;
+    this.rawList;
+    this.objectList;
+
+    this.convertIntoListOfItems = function(inputFromEditView) {
+      this.inputContent = inputFromEditView;
+      this.objectList = [];
+      this.rawList = this.inputContent.split("[]");
+      // remove the first empty item
+      this.rawList.shift();
+      for(item in this.rawList) {
+        this.objectList.push({text: this.rawList[item], done:false});
       }
-
-      ngModel.$render = function() {
-        element.html(ngModel.$viewValue || "");
-      };
-
-      element.bind("blur keyup change", function() {
-        scope.$apply(read);
-      });
     }
-  };
-});
+}]);
